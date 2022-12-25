bb471d5047b430297e99a328dc1f4d522c05340e
Laurent Jouanneau <dev@ljouanneau.com>
2022-12-25 23:59:31 +0100

    [CI] Fix build of vendor

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 source /etc/profile.d/php_vars.sh
 php booster/install/configurator.php -v
 php booster/install/installer.php -v
+echo "Removing temp directory..."
 sudo rm -rf /var/cache/sites/booster.jelix.org/www/
